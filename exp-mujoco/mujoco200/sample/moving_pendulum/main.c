949c315a462e1d232cdd58eae67b9bb8c999bc1c
ksdngz <dummy>
2023-03-15 02:14:18 +0900

    added for moving pendulum.

--- exp-mujoco/mujoco200/sample/moving_pendulum/main.c
+++ exp-mujoco/mujoco200/sample/moving_pendulum/main.c
@@ -11,7 +11,7 @@
 #include "string.h"
 
 //simulation end time
-double simend = 5;
+double simend = 30;
 
 #define ndof 2
 
@@ -22,9 +22,10 @@ const int data_frequency = 50; //frequency at which data is written to a file
 
 
 // char xmlpath[] = "../myproject/dbpendulum/doublependulum.xml";
-char xmlpath[] = "../sample/dbpendulum/doublependulum.xml";
+// char xmlpath[] = "../sample/dbpendulum/doublependulum.xml";
+char xmlpath[] = "../sample/moving_pendulum/movingPendulum.xml";
 //char datapath[] = "../myproject/dbpendulum/data.csv";
-char datapath[] = "../sample/dbpendulum/data.csv";
+char datapath[] = "../sample/moving_pendulum/data.csv";
 
 
 //Change the path <template_writeData>
@@ -156,86 +157,91 @@ void save_data(const mjModel* m, mjData* d)
 //**************************
 void mycontroller(const mjModel* m, mjData* d)
 {
+/*
   //write control here
   mj_energyPos(m,d);
   mj_energyVel(m,d);
   //printf("%f %f %f %f \n",d->time,d->energy[0],d->energy[1],d->energy[0]+d->energy[1]);
+*/
+//  //check equations
+//  //M*qacc + qfrc_bias = qfrc_applied + ctrl
+//  //M*qddot + f = qfrc_applied + ctrl
+//  //const int ndof = 2;
+//  double dense_M[ndof*ndof] = {0};
+//  mj_fullM(m,dense_M, d->qM);
+//  double M[ndof][ndof]={0};
+//  M[0][0] = dense_M[0];
+//  M[0][1] = dense_M[1];
+//  M[1][0] = dense_M[2];
+//  M[1][1] = dense_M[3];
+//  // printf("%f %f \n",M[0][0],M[0][1]);
+//  // printf("%f %f \n",M[1][0],M[1][1]);
+//  // printf("******** \n");
+//
+//  double qddot[ndof]={0};
+//  qddot[0]=d->qacc[0];
+//  qddot[1]=d->qacc[1];
+//
+//  double f[ndof]={0};
+//  f[0] = d->qfrc_bias[0];
+//  f[1] = d->qfrc_bias[1];
+//
+//  double lhs[ndof]={0};
+//  mju_mulMatVec(lhs,dense_M,qddot,2,2); //lhs = M*qddot
+//  lhs[0] = lhs[0] + f[0]; //lhs = M*qddot + f
+//  lhs[1] = lhs[1] + f[1];
+//
+//  d->qfrc_applied[0] = 0.1*f[0];
+//  d->qfrc_applied[1] = 0.5*f[1];
+//
+//  double rhs[ndof]={0};
+//  rhs[0] = d->qfrc_applied[0];
+//  rhs[1] = d->qfrc_applied[1];
+//
+//  // printf("%f %f \n",lhs[0], rhs[0]);
+//  // printf("%f %f \n",lhs[1], rhs[1]);
+//  // printf("******\n");
+//
+//  //control
+//  double Kp1 = 100, Kp2 = 100;
+//  double Kv1 = 10, Kv2 = 10;
+//  double qref1 = -0.5, qref2 = -1.6;
+//
+//  //PD control
+//  // d->qfrc_applied[0] = -Kp1*(d->qpos[0]-qref1)-Kv1*d->qvel[0];
+//  // d->qfrc_applied[1] = -Kp2*(d->qpos[1]-qref2)-Kv2*d->qvel[1];
+//
+//  //coriolis + gravity + PD control
+//  // d->qfrc_applied[0] = f[0]-Kp1*(d->qpos[0]-qref1)-Kv1*d->qvel[0];
+//  // d->qfrc_applied[1] = f[1]-Kp2*(d->qpos[1]-qref2)-Kv2*d->qvel[1];
+//
+//  //Feedback linearization
+//  //M*(-kp( ... ) - kv(...) + f)
+//  double tau[2]={0};
+//  tau[0]=-Kp1*(d->qpos[0]-qref1)-Kv1*d->qvel[0];
+//  tau[1]=-Kp2*(d->qpos[1]-qref2)-Kv2*d->qvel[1];
+//
+//  mju_mulMatVec(tau,dense_M,tau,2,2); //lhs = M*tau
+//  tau[0] += f[0];
+//  tau[1] += f[1];
+//    d->qfrc_applied[0] = tau[0];
+//    d->qfrc_applied[1] = tau[1];
+//
+//
+//
+//
+//
+//
+//  //write data here (dont change/dete this function call; instead write what you need to save in save_data)
+//  if ( loop_index%data_frequency==0)
+//    {
+//      save_data(m,d);
+//    }
+//  loop_index = loop_index + 1;
+//
+//
 
-  //check equations
-  //M*qacc + qfrc_bias = qfrc_applied + ctrl
-  //M*qddot + f = qfrc_applied + ctrl
-  //const int ndof = 2;
-  double dense_M[ndof*ndof] = {0};
-  mj_fullM(m,dense_M, d->qM);
-  double M[ndof][ndof]={0};
-  M[0][0] = dense_M[0];
-  M[0][1] = dense_M[1];
-  M[1][0] = dense_M[2];
-  M[1][1] = dense_M[3];
-  // printf("%f %f \n",M[0][0],M[0][1]);
-  // printf("%f %f \n",M[1][0],M[1][1]);
-  // printf("******** \n");
 
-  double qddot[ndof]={0};
-  qddot[0]=d->qacc[0];
-  qddot[1]=d->qacc[1];
-
-  double f[ndof]={0};
-  f[0] = d->qfrc_bias[0];
-  f[1] = d->qfrc_bias[1];
-
-  double lhs[ndof]={0};
-  mju_mulMatVec(lhs,dense_M,qddot,2,2); //lhs = M*qddot
-  lhs[0] = lhs[0] + f[0]; //lhs = M*qddot + f
-  lhs[1] = lhs[1] + f[1];
-
-  d->qfrc_applied[0] = 0.1*f[0];
-  d->qfrc_applied[1] = 0.5*f[1];
-
-  double rhs[ndof]={0};
-  rhs[0] = d->qfrc_applied[0];
-  rhs[1] = d->qfrc_applied[1];
-
-  // printf("%f %f \n",lhs[0], rhs[0]);
-  // printf("%f %f \n",lhs[1], rhs[1]);
-  // printf("******\n");
-
-  //control
-  double Kp1 = 100, Kp2 = 100;
-  double Kv1 = 10, Kv2 = 10;
-  double qref1 = -0.5, qref2 = -1.6;
-
-  //PD control
-  // d->qfrc_applied[0] = -Kp1*(d->qpos[0]-qref1)-Kv1*d->qvel[0];
-  // d->qfrc_applied[1] = -Kp2*(d->qpos[1]-qref2)-Kv2*d->qvel[1];
-
-  //coriolis + gravity + PD control
-  // d->qfrc_applied[0] = f[0]-Kp1*(d->qpos[0]-qref1)-Kv1*d->qvel[0];
-  // d->qfrc_applied[1] = f[1]-Kp2*(d->qpos[1]-qref2)-Kv2*d->qvel[1];
-
-  //Feedback linearization
-  //M*(-kp( ... ) - kv(...) + f)
-  double tau[2]={0};
-  tau[0]=-Kp1*(d->qpos[0]-qref1)-Kv1*d->qvel[0];
-  tau[1]=-Kp2*(d->qpos[1]-qref2)-Kv2*d->qvel[1];
-
-  mju_mulMatVec(tau,dense_M,tau,2,2); //lhs = M*tau
-  tau[0] += f[0];
-  tau[1] += f[1];
-    d->qfrc_applied[0] = tau[0];
-    d->qfrc_applied[1] = tau[1];
-
-
-
-
-
-
-  //write data here (dont change/dete this function call; instead write what you need to save in save_data)
-  if ( loop_index%data_frequency==0)
-    {
-      save_data(m,d);
-    }
-  loop_index = loop_index + 1;
 }
 
 
@@ -313,6 +319,7 @@ int main(int argc, const char** argv)
     fid = fopen(datapath,"w");
     init_save_data();
 
+    //d->qpos[0] = 1;
     d->qpos[0] = 0.5;
     //d->qpos[1] = 0;
     // use the first while condition if you want to simulate for a period.
