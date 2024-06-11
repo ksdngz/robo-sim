- fixed rotation order of euler angle - modified constant class in common

exp-py/panda-arm/debugger/debugger.py
@@ -144,11 +144,11 @@ class Debugger:
         self.__allJointsRequest.put(tr.MultiJointMoveRequest(targets))
 
     def __onbtn_moveZero(self):
-        targets : list[tuple[int, float]] = [(i+1, np.rad2deg(j)) for i,j in enumerate(const.Constants.ZERO_JOINTS)]
+        targets : list[tuple[int, float]] = [(i+1, np.rad2deg(j)) for i,j in enumerate(const.ZERO_JOINTS)]
         self.__allJointsRequest.put(tr.MultiJointMoveRequest(targets))
 
     def __onbtn_moveHome(self):
-        hjoint = copy.copy(const.Constants.HOME_JOINTS)  
+        hjoint = copy.copy(const.HOME_JOINTS)  
         #hjoint = [0, -np.pi / 4, 0, -3 * np.pi / 4, 0, np.pi / 2, np.pi / 4]  
         targets : list[tuple[int, float]] = [(i+1, np.rad2deg(j)) for i,j in enumerate(hjoint)]
         self.__allJointsRequest.put(tr.MultiJointMoveRequest(targets))

exp-py/panda-arm/panda_arm.py
@@ -190,7 +190,7 @@ gCount = 0
 initq_deg = [0, 10, 0, -150, 0, 180, 0]
 #initq = [0., 1.3, 0., 0., 0., 0., 0.]
 #initq = [np.deg2rad(q) for q in initq_deg]
-initq =copy.copy(const.Constants.HOME_JOINTS)
+initq =copy.copy(const.HOME_JOINTS)
 #print(np.rad2deg(initq))
 #get the full path
 dirname = os.path.dirname(__file__)

exp-py/panda-arm/simState.py
@@ -66,10 +66,18 @@ class SimState:
         #index_tcp = mj.mj_name2id(model, mj.mjtObj.mjOBJ_BODY, 'panda_link7')
         index_tcp = 7 # todo to be refactored
         # to do change from quat to rpy
-        rpy = [0]*3        
-        quat = np.array(data.xquat[index_tcp])
-        rot = Rotation.from_quat(quat)
-        angle = rot.as_euler('ZYX', degrees=True)        
+#        rpy = [0]*3        
+#        xmat = np.array(data.xmat[index_tcp])
+        quat = np.array(data.xquat[index_tcp]) # q = (w, x, y, z).
+        q_w = quat[0]
+        q_x = quat[1]
+        q_y = quat[2]
+        q_z = quat[3]
+        newQ = np.array([q_x, q_y, q_z, q_w])
+#        rot = Rotation.from_matrix(xmat)
+        rot = Rotation.from_quat(newQ) # x, y, z, w
+        angle = rot.as_euler('zyx', degrees=True)        
+#        angle = rot.as_euler('ZYX', degrees=True)        
         self.__tcp.update(data.xpos[index_tcp], angle)        
         self.dataLogger.log(self.time(), self.qs(), self.qdots(), self.qcmds(), self.qdotcmds())        
 

exp-py/panda-arm/task/taskManager.py
@@ -100,9 +100,14 @@ class TaskManager:
                 ro = np.deg2rad(tcpTarget[3])
                 pi = np.deg2rad(tcpTarget[4])
                 ya = np.deg2rad(tcpTarget[5])
-                rot = SE3.RPY(ro,pi,ya)                
+#                rot = SE3.RPY(ro,pi,ya)
+                gamma = [ro,pi,ya]
+                rot = SE3.RPY(gamma, order="xyz")
+#                rot = SE3.RPY(gamma, order="zyx")
+                          
                 trans = SE3.Trans(x,y,z)
                 target = trans * rot
+                print(target)
                 q0 : np.ndarray = np.array(np.deg2rad(self.__simState.qs())) # [rad]
                 q : np.ndarray = rtb.inverseKin(target, q0) # [rad]
                 jntTarget : list[float] = np.rad2deg(q) #[deg]