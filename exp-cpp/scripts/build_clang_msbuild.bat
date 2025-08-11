c31c995353624dbbeaefba0af8e7cbd0c50bd81c
daisukematsuanga <dd>
2025-08-11 23:23:17 +0900

    Removed build.bat due to bootstrap

--- exp-cpp/scripts/build_clang_msbuild.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM Build with MSBuild and Clang (using Visual Studio Generator)
-setlocal
-if not exist build mkdir build
-cd build
-cmake -G "Visual Studio 17 2022" -T ClangCL ..
-cmake --build .
-endlocal
