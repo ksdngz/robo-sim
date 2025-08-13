9f007d088e2f9398a6fb9a6b468b671881315f50
daisukematsuanga <dd>
2025-08-13 15:32:15 +0900

    Add drawing sph function

--- exp-cpp/samples/simple_sim.cpp
+++ exp-cpp/samples/simple_sim.cpp
@@ -8,45 +8,82 @@
 #include <GLFW/glfw3.h>
 #include <mujoco/mujoco.h>
 
-mjModel* m = NULL;
-mjData* d = NULL;
-mjvCamera cam;
-mjvOption opt;
-mjvScene scn;
-mjrContext con;
-bool button_left = false;
-bool button_middle = false;
-bool button_right = false;
-double lastx = 0;
-double lasty = 0;
+
+class MjSim
+{
+public:
+	MjSim(){}
+	mjModel* m = NULL;
+	mjData* d = NULL;
+	mjvCamera cam;
+	mjvOption opt;
+	mjvScene scn;
+	mjrContext con;
+	bool button_left = false;
+	bool button_middle = false;
+	bool button_right = false;
+	double lastx = 0;
+	double lasty = 0;
+};
+MjSim mj;
 
 using Position = std::array<double, 3>;
 using WayPoints = std::vector<Position>;
 
+void drawSph(
+	MjSim& mj,
+	const Position& pt,
+	double radius,
+	float rgba[4])
+{ 
+	mjvScene& scn(mj.scn);
+    // add a decorative geometry
+    mjtNum sphsize[3] = {radius, 0, 0};
+//    mjtNum sphpos[3] = {0., 0., 0.};
+    mjtNum myrot3x3[9] = {1., 0., 0., 0., 1., 0., 0., 0., 1.};
+
+//    mju_copy3(sphpos, pt);
+
+    // one more geom to render
+    //scn.ngeom = scn.ngeom + 1;
+    // mygeom now points to the last location in geoms buffer
+    mjvGeom *mygeom = scn.geoms + scn.ngeom;
+    // decor geom
+    mygeom->objtype = mjOBJ_UNKNOWN;
+    mygeom->objid = -1;
+    mygeom->category = mjCAT_DECOR;
+    mygeom->segid = scn.ngeom;
+
+    // Add it to the scene
+    mjv_initGeom(mygeom, mjGEOM_SPHERE, sphsize, pt.data(), myrot3x3, rgba);
+    mjv_addGeoms(mj.m, mj.d, &mj.opt, NULL, mjCAT_DECOR, &scn);
+	scn.ngeom++;
+}
+
 int drawLine(
-	mjvScene& scn,
+	MjSim& mj,
 	const Position& from,
 	const Position& to,
 	float rgba[4])
 {
-	if ( scn.ngeom>=scn.maxgeom ) {
-		mj_warning(d, mjWARN_VGEOMFULL, scn.maxgeom);
+	if ( mj.scn.ngeom>=mj.scn.maxgeom ) {
+		mj_warning(mj.d, mjWARN_VGEOMFULL, mj.scn.maxgeom);
 		return EXIT_FAILURE;
 	}
-	mjvGeom* g = scn.geoms + scn.ngeom;
+	mjvGeom* g = mj.scn.geoms + mj.scn.ngeom;
 	memset(g, 0, sizeof(mjvGeom));
 	mjv_initGeom(g, mjGEOM_NONE, NULL, NULL, NULL, rgba);
 	g->objtype = mjOBJ_UNKNOWN;
 	g->objid = 0;
 	g->category = mjCAT_DECOR;
-	g->segid = scn.ngeom;
+	g->segid = mj.scn.ngeom;
 	mjv_connector(g, mjGEOM_LINE, 0.05, from.data(), to.data());
-	scn.ngeom++;
+	mj.scn.ngeom++;
 	return EXIT_SUCCESS;
 }
 
 int drawSpline(
-	mjvScene& scn,
+	MjSim& mj,
 	const WayPoints& wp,
 	float rgba[4]
 ) {
@@ -74,63 +111,63 @@ int drawSpline(
 					(2.0*p0[k] - 5.0*p1[k] + 4.0*p2[k] - p3[k]) * t * t +
 					(-p0[k] + 3.0*p1[k] - 3.0*p2[k] + p3[k]) * t * t * t);
 			}
-			if (drawLine(scn, prev, pt, rgba) != EXIT_SUCCESS) err = EXIT_FAILURE;
+			if (drawLine(mj, prev, pt, rgba) != EXIT_SUCCESS) err = EXIT_FAILURE;
 			prev = pt;
 		}
 	}
 	return err;
 }
 
-
 void keyboard(GLFWwindow* window, int key, int scancode, int act, int mods) {
 	if (act==GLFW_PRESS && key==GLFW_KEY_BACKSPACE) {
-		mj_resetData(m, d);
-		mj_forward(m, d);
+		mj_resetData(mj.m, mj.d);
+		mj_forward(mj.m, mj.d);
 	}
 }
 
 void mouse_button(GLFWwindow* window, int button, int act, int mods) {
-	button_left = (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT)==GLFW_PRESS);
-	button_middle = (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_MIDDLE)==GLFW_PRESS);
-	button_right = (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT)==GLFW_PRESS);
-	glfwGetCursorPos(window, &lastx, &lasty);
+	mj.button_left = (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT)==GLFW_PRESS);
+	mj.button_middle = (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_MIDDLE)==GLFW_PRESS);
+	mj.button_right = (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT)==GLFW_PRESS);
+	glfwGetCursorPos(window, &mj.lastx, &mj.lasty);
 }
 
 void mouse_move(GLFWwindow* window, double xpos, double ypos) {
-	if (!button_left && !button_middle && !button_right) return;
-	double dx = xpos - lastx;
-	double dy = ypos - lasty;
-	lastx = xpos;
-	lasty = ypos;
+	if (!mj.button_left && !mj.button_middle && !mj.button_right) return;
+	double dx = xpos - mj.lastx;
+	double dy = ypos - mj.lasty;
+	mj.lastx = xpos;
+	mj.lasty = ypos;
 	int width, height;
 	glfwGetWindowSize(window, &width, &height);
 	bool mod_shift = (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT)==GLFW_PRESS ||
 										glfwGetKey(window, GLFW_KEY_RIGHT_SHIFT)==GLFW_PRESS);
 	mjtMouse action;
-	if (button_right) action = mod_shift ? mjMOUSE_MOVE_H : mjMOUSE_MOVE_V;
-	else if (button_left) action = mod_shift ? mjMOUSE_ROTATE_H : mjMOUSE_ROTATE_V;
+	if (mj.button_right) action = mod_shift ? mjMOUSE_MOVE_H : mjMOUSE_MOVE_V;
+	else if (mj.button_left) action = mod_shift ? mjMOUSE_ROTATE_H : mjMOUSE_ROTATE_V;
 	else action = mjMOUSE_ZOOM;
-	mjv_moveCamera(m, action, dx/height, dy/height, &scn, &cam);
+	mjv_moveCamera(mj.m, action, dx/height, dy/height, &mj.scn, &mj.cam);
 }
 
 void scroll(GLFWwindow* window, double xoffset, double yoffset) {
-	mjv_moveCamera(m, mjMOUSE_ZOOM, 0, -0.05*yoffset, &scn, &cam);
+	mjv_moveCamera(mj.m, mjMOUSE_ZOOM, 0, -0.05*yoffset, &mj.scn, &mj.cam);
 }
 
 int main(int argc, const char** argv) {
+
 	// モデルファイルパスを決定
 	const char* model_path = "../models/urdf/robot/panda_arm_mjcf.xml";
 	if (argc == 2) model_path = argv[1];
 
 	// load and compile model
 	char error[1000] = "Could not load model";
-	m = mj_loadXML(model_path, 0, error, 1000);
-	if (!m) {
+	mj.m = mj_loadXML(model_path, 0, error, 1000);
+	if (!mj.m) {
 		mju_error("Load model error: %s", error);
 	}
 
 	// make data
-	d = mj_makeData(m);
+	mj.d = mj_makeData(mj.m);
 
 	// init GLFW
 	if (!glfwInit()) {
@@ -143,15 +180,15 @@ int main(int argc, const char** argv) {
 	glfwSwapInterval(1);
 
 	// initialize visualization data structures
-	mjv_defaultCamera(&cam);
-	mjv_defaultOption(&opt);
-	mjv_defaultScene(&scn);
-	mjr_defaultContext(&con);
+	mjv_defaultCamera(&mj.cam);
+	mjv_defaultOption(&mj.opt);
+	mjv_defaultScene(&mj.scn);
+	mjr_defaultContext(&mj.con);
 
 	// create scene and context
-	mjv_makeScene(m, &scn, 2000);
-	scn.flags[mjCAT_DECOR] = 1;
-	mjr_makeContext(m, &con, mjFONTSCALE_150);
+	mjv_makeScene(mj.m, &mj.scn, 2000);
+	mj.scn.flags[mjCAT_DECOR] = 1;
+	mjr_makeContext(mj.m, &mj.con, mjFONTSCALE_150);
 
 	// install GLFW mouse and keyboard callbacks
 	glfwSetKeyCallback(window, keyboard);
@@ -161,15 +198,15 @@ int main(int argc, const char** argv) {
 
 	// run main loop, target real-time simulation and 60 fps rendering
 	while (!glfwWindowShouldClose(window)) {
-		mjtNum simstart = d->time;
-		while (d->time - simstart < 1.0/60.0) {
-			mj_step(m, d);
+		mjtNum simstart = mj.d->time;
+		while (mj.d->time - simstart < 1.0/60.0) {
+			mj_step(mj.m, mj.d);
 		}
 		mjrRect viewport = {0, 0, 0, 0};
 		glfwGetFramebufferSize(window, &viewport.width, &viewport.height);
 
 		// Update the scene first (this resets scn.ngeom)
-		mjv_updateScene(m, d, &opt, NULL, &cam, mjCAT_ALL, &scn);
+		mjv_updateScene(mj.m, mj.d, &mj.opt, NULL, &mj.cam, mjCAT_ALL, &mj.scn);
 
 		// add line
 		WayPoints wp = {
@@ -177,21 +214,26 @@ int main(int argc, const char** argv) {
 			{1.0, 0.0, 0.0},
 			{1.0, 0.5, 0.2}};
 		float rgba[4] = {1.f, 0.f, 1.f, 0.9f};
-		int errCode = drawSpline(scn, wp, rgba);
+		int errCode = drawSpline(mj, wp, rgba);
 		if (errCode != EXIT_SUCCESS){
 		  return errCode;
 		}
+		// add sphere
+		Position pt = {1.0, 0.0, 0.0};
+		double radius = 0.03;
+		float clr_sph[4] = {1.f, 0.f, 0.f, 1.f};
+		drawSph(mj, pt, radius, clr_sph);
 
-		mjr_render(viewport, &scn, &con);
+		mjr_render(viewport, &mj.scn, &mj.con);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
 	//free visualization storage
-	mjv_freeScene(&scn);
-	mjr_freeContext(&con);
-	mj_deleteData(d);
-	mj_deleteModel(m);
+	mjv_freeScene(&mj.scn);
+	mjr_freeContext(&mj.con);
+	mj_deleteData(mj.d);
+	mj_deleteModel(mj.m);
 #if defined(__APPLE__) || defined(_WIN32)
 	glfwTerminate();
 #endif
