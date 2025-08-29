8230270a6e07b26fd129a793d56779f42bfac168
ksdngz <dummy>
2025-08-30 00:29:58 +0900

    重複によるリンクエラー修正

--- exp-cpp/samples/constraint_path_planner.cpp
+++ exp-cpp/samples/constraint_path_planner.cpp
@@ -6,8 +6,8 @@
 #include <iostream>
 #include <cmath>
 
-#include "./mj_sim.hpp"
-#include "./constraint_path_planner.hpp"
+#include "mj_sim.hpp"
+#include "constraint_path_planner.hpp"
 
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
--- exp-cpp/samples/path_planner.cpp
+++ exp-cpp/samples/path_planner.cpp
@@ -6,7 +6,7 @@
 #include <vector>
 #include <iostream>
 #include <cmath>
-#include "./path_planner.hpp"
+#include "path_planner.hpp"
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
